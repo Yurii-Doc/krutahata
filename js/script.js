a40242bc5d55b2c9308842bff47c0f7bdc27289c
Yurii-Doc <63263527+Yurii-Doc@users.noreply.github.com>
2021-02-05 00:48:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,67 @@
 $(document).ready(function () {
-  $('.foto-slider__img').magnificPopup({
-    type: 'image',
-    //gallery: {
-    //enabled: true
-    //},
-  });
   document.querySelector('.icon-menu, .menu__body').onclick = function () {
     document.querySelector('.icon-menu').classList.toggle('active');
     document.querySelector('.menu__body').classList.toggle('active');
     document.querySelector('body').classList.toggle('lock');
   }
- 
+
+  //прокрутка (скрол)
+
+  const animItems = document.querySelectorAll("._anim-items");
+
+  if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(params) {
+      for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - awindow.innerHeight / animStart
+        }
+
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+          animItem.classList.add("_active");
+        } else {
+          if (!animItem.classList.contains('_anim-no-hide')) {    //отключаем повторную прокрутку(появление скрола)//
+            animItem.classList.remove("_active");                 //повторного анимирования//
+          }
+        }
+      }
+    }
+    function offset(el) {
+      const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, Left: rect.Left + scrollLeft }
+    }
+
+    setTimeout(() => {      //задержка вызова функции//
+      animOnScroll();
+    }, 400);
+
+  }
+
+  //плавной прокрутки//
+  $("a.page-scroll").bind("click", function (e) {
+    e.preventDefault();
+    var anchor = $(this).attr('href');
+    $('html, body').stop().animate({
+      scrollTop: $(anchor).offset().top - 0
+    }, 1500);
+  });
+
+
   // Closes the Responsive Menu on Menu Item Click
-  //Закрывает отзывчивое меню при нажатии пункта меню
+  // Закрывает отзывчивое меню при нажатии пункта меню
   $('.menu__body ul li a').click(function () {
     $('.icon-menu:visible').click();
   });
 
-$('.item-slider').slick({
+  $('.item-slider').slick({
     arrows: false,
     dots: true,
     slidesToShow: 4,
@@ -56,5 +100,11 @@ $('.item-slider').slick({
         }
       }
     ]
+  });
+  $('.foto-slider__img').magnificPopup({
+    type: 'image',
+    //gallery: {
+    //enabled: true
+    //},
   });
 });
